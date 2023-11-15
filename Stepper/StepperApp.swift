6433379f15223e6b37ce0d8d7f5c9289e357f5cb
Lexi Harrop <lharrop@lcs.on.ca>
2023-11-14 19:18:13 -0500

    Initial Commit

---
+++ Stepper/StepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperApp.swift
+//  Stepper
+//
+//  Created by Lexi on 2023-11-14.
+//
+
+import SwiftUI
+
+@main
+struct StepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
